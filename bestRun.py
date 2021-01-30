2aefdb15f99e7ea99fba3d32746d9148a74bb91a
alihussainia <makasbati@gmail.com>
2021-01-30 05:45:18 +0500

    Adding files

---
+++ bestRun.py
@@ -0,0 +1,16 @@
+# Details for the best run AutoML model 
+from pprint import pprint
+def print_model(model, prefix=""):
+    for step in model.steps:
+        print(prefix + step[0])
+        if hasattr(step[1], 'estimators') and hasattr(step[1], 'weights'):
+            pprint({'estimators': list(
+                e[0] for e in step[1].estimators), 'weights': step[1].weights})
+            print()
+            for estimator in step[1].estimators:
+                print_model(estimator[1], estimator[0] + ' - ')
+        else:
+            pprint(step[1].get_params())
+            print()
+
+# print_model(best_automl_model)
